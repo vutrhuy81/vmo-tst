9f3b303ebda4e62c785f614fb1be1255a7f3a634
Tran Huy Vu <vutrhuy81@gmail.com>
2026-09-13 14:24:51 +0700

    Add new user 'test1' to default users

--- auth.js
+++ auth.js
@@ -15,8 +15,14 @@ const VMOAuth = (() => {
       name: 'Quản trị viên tối cao',
       createdAt: '2026-09-01T00:00:00.000Z'
     },
-      username: 'hoangkien',
+    { username: 'hoangkien',
       password: '123456',
+      role: 'admin',
+      name: 'Quản trị viên tối cao',
+      createdAt: '2026-09-01T00:00:00.000Z'
+    },
+    { username: 'test1',
+      password: 'password@123',
       role: 'admin',
       name: 'Quản trị viên tối cao',
       createdAt: '2026-09-01T00:00:00.000Z'
