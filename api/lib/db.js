c5fc945eb0762b7ff42ecd2cbe628827e35c178e
Tran Huy Vu <vutrhuy81@gmail.com>
2026-09-15 10:35:52 +0700

    Update db.js

--- api/lib/db.js
+++ api/lib/db.js
@@ -1,1 +1,27 @@
+const { MongoClient } = require('mongodb');
 
+const uri = process.env.MONGODB_URI;
+let client;
+let clientPromise;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Thiếu biến môi trường MONGODB_URI!');
+}
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+async function getDb() {
+  const conn = await clientPromise;
+  return conn.db();
+}
+
+module.exports = { getDb };
